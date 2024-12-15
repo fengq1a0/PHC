cc014d9eeb8b923f31fcfa2d59ec21b32dd61f27
fengq1a0 <fengq1a0@foxmail.com>
2024-12-15 12:42:14 -0500

    clean the code

--- train_yiming_amass.sh
+++
@@ -1,8 +0,0 @@
-export LD_LIBRARY_PATH="/mnt/kostas-graid/sw/envs/fengqiao/miniconda3/envs/isaac/lib:$LD_LIBRARY_PATH"
-python phc/run_hydra.py learning=im_big exp_name=amass_prim env=env_im robot=smpl_humanoid \
- env.motion_file=data/amass_smpl/train.pkl \
- epoch=6000 resume_str=hmxodne1
-
-# Used to pre-train
-# change epoch to resume
-# change resume_str to continue wandb Log
