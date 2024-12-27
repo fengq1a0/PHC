91f741d2c88737d5279031167cf976161ede0979
Qiao Feng <1459949343@qq.com>
2024-12-27 14:32:24 -0500

    fix data preprocess

--- scripts/data_process/FQ_img.py
+++ scripts/data_process/FQ_img.py
@@ -21,6 +21,7 @@ from smplx import SMPL
 
 
 if __name__ == "__main__":
+    debug_data = False
     #----------------------------------------------------
     process_split = "train"
     upright_start = True
@@ -48,11 +49,12 @@ if __name__ == "__main__":
     #------------------------------------------------------
     base_path = "/home/fq/data/behave/processed/"
     with open("/home/fq/data/behave/scripts/data_list.txt", "r") as fi:
-        name_list = fi.read().split()[:25]
-#    name_list = [
-#        "Date01_Sub01_boxsmall_hand",
-#    ]
-    ground_normals = np.load("/home/fq/data/behave/grounds/normals.npy")
+        name_list = fi.read().split()# [:30]
+    if debug_data:
+        name_list = [
+            "Date01_Sub01_boxsmall_hand",
+        ]
+    ground_normals = np.load("/home/fq/data/behave/grounds/planes.npy")
     smpl = SMPL(model_path="/home/fq/repo/PHC/data/smpl/", gender="neutral")
     smpl_2_mujoco = [SMPL_BONE_ORDER_NAMES.index(q) for q in SMPL_MUJOCO_NAMES if q in SMPL_BONE_ORDER_NAMES]
     #------------------------------------------------------
@@ -66,6 +68,10 @@ if __name__ == "__main__":
         trans = smpl_motion['trans'].astype(np.float64)
         poses = smpl_motion['poses'].astype(np.float64)
         betas = smpl_motion['betas'].astype(np.float64)
+        if debug_data:
+            trans = smpl_motion['trans'].astype(np.float64)[:150]
+            poses = smpl_motion['poses'].astype(np.float64)[:150]
+            betas = smpl_motion['betas'].astype(np.float64)[:150]
 
         with torch.no_grad():
             j0 = smpl(
@@ -76,7 +82,7 @@ if __name__ == "__main__":
 
         g_m = ground_normals[int(name[5])]
         y_m = g_m[:3]
-        p_m = np.array([0, -g_m[3] / g_m[1], 0])
+        p_m = np.array([0, -g_m[3] / g_m[1], 0, 1])
 
         Ti = np.array([
             [1,  0,  0, 0],
@@ -84,13 +90,18 @@ if __name__ == "__main__":
             [0,  0, -1, 0],
             [0,  0,  0, 1]
         ])
-        RRRR = np.array([[-1.0,  0.0,  0.0, 0],
-                         [ 0.0,  0.0,  1.0, 0], 
+        RRRR = np.array([[ 1.0,  0.0,  0.0, 0],
+                         [ 0.0,  0.0, -1.0, 0], 
                          [ 0.0,  1.0,  0.0, 0],
                          [   0,    0,    0, 1]])
         
-        for i in range(4):
-
+        if debug_data:
+            views = [1]
+        else:
+            views = [0, 1, 2, 3]
+        for i in views:
+            #########################################################################################################
+            # For FQ features
             with open(os.path.join(data_path, "intri/%d/calibration.json" % i), "r") as tmp:
                 data = json.load(tmp)
                 cx =    np.array(data["color"]["cx"])
@@ -100,18 +111,18 @@ if __name__ == "__main__":
                 width = np.array(data["color"]["width"])
 
             image_feature = np.load(os.path.join(data_path, "fq_info_%d.npz" % i))
+            
             # Regulize kp2d and bbox
-            # then concat
             bbox = image_feature["bbox"].copy()
             bbox[:,0] = (bbox[:,0]-cx) / fx *2.8
             bbox[:,1] = (bbox[:,1]-cy) / fy *2.8
-            bbox[:,2] = (bbox[:,2] - 0.12 * (fx+fy)) / (0.03 * (fx + fy))
+            bbox[:,2] = (bbox[:,2] - 0.12 * (fx+fy)) / (0.3 * (fx + fy)) # 0.03
             kp2d = image_feature["kp2d"].copy()
             kp2d[:,:,0] = (kp2d[:,:,0]-cx) / fx *2.8
             kp2d[:,:,1] = (kp2d[:,:,1]-cy) / fy *2.8
             img_feat = image_feature["img_feat"]
-
-
+            #########################################################################################################
+            # For T_m2c, T_c2g
             with open(os.path.join(data_path, "extri/%d/config.json" % i), "r") as tmp:
                 data = json.load(tmp)
                 R = np.array(data["rotation"]).reshape(3,3)
@@ -119,38 +130,55 @@ if __name__ == "__main__":
                 T_m2c = np.eye(4)
                 T_m2c[:3,:3] = R
                 T_m2c[:3,3] = T
+                T_m2c = np.linalg.inv(T_m2c)
                 T_m2c = Ti@T_m2c@Ti
             
             y_c = T_m2c[:3,:3] @ y_m
+            y_c = y_c / np.linalg.norm(y_c)
             x_c = np.cross(y_c, np.array([0.0, 0.0, 1.0]))
+            x_c = x_c / np.linalg.norm(x_c)
             z_c = np.cross(x_c, y_c)
-            R_c2g = np.array([x_c, y_c, z_c]).T
+            z_c = z_c / np.linalg.norm(z_c)
+            R_c2g = np.array([x_c, y_c, z_c])
 
-            p_g = R_c2g @ T_m2c[:3,:3] @ p_m
+            p_g = R_c2g @ (T_m2c @ p_m)[:3]
             T_c2g = np.eye(4)
             T_c2g[:3,:3] = R_c2g
             T_c2g[1,3] = - p_g[1]
             #########################################################################################################
+            # For isaac gym:
+            # poses_isaac, trans_isaac, betas
             T_m2g = RRRR @ T_c2g @ T_m2c
-            poses_aa = poses.copy()
 
-            root_rot = sRot.from_rotvec(poses_aa[:,:3]).as_matrix()
-            root_aa  = sRot.from_matrix(T_m2g[:3,:3] @ root_rot).as_rotvec()
-            poses_aa[:,:3] = root_aa
+            poses_isaac = poses.copy()
+            root_rot = sRot.from_rotvec(poses_isaac[:,:3]).as_matrix()
+            poses_isaac[:,:3] = sRot.from_matrix(T_m2g[:3,:3] @ root_rot).as_rotvec()
 
-            new_trans = (trans + j0) @ T_m2g[:3,:3].T + T_m2g[:3,3] - j0
+            trans_isaac = (trans + j0) @ T_m2g[:3,:3].T + T_m2g[:3,3] - j0
 
-            # poses_aa, new_trans, betas
+
+            # For original smpl:
+            # poses_g, trans_g, betas
+            T_m2g = T_c2g @ T_m2c
+
+            poses_g = poses.copy()
+            root_rot = sRot.from_rotvec(poses_g[:,:3]).as_matrix()
+            poses_g[:,:3] = sRot.from_matrix(T_m2g[:3,:3] @ root_rot).as_rotvec()
+
+            trans_g = (trans + j0) @ T_m2g[:3,:3].T + T_m2g[:3,3] - j0
             #########################################################################################################
-            N = poses_aa.shape[0]
+            N = min(poses_isaac.shape[0], kp2d.shape[0])
+            poses_isaac = poses_isaac[:N]
+            trans_isaac = trans_isaac[:N]
+
             # to mujoco
-            pose_aa_mj = poses_aa.reshape(N, 24, 3)[:, smpl_2_mujoco]
+            pose_aa_mj = poses_isaac.reshape(N, 24, 3)[:, smpl_2_mujoco]
             pose_quat = sRot.from_rotvec(pose_aa_mj.reshape(-1, 3)).as_quat().reshape(N, 24, 4)
             # smpl_local_robot?
             smpl_local_robot.load_from_skeleton(betas=torch.from_numpy(betas), gender=[0], objs_info=None)
             smpl_local_robot.write_xml(f"phc/data/assets/mjcf/{robot_cfg['model']}_humanoid.xml")
             skeleton_tree = SkeletonTree.from_mjcf(f"phc/data/assets/mjcf/{robot_cfg['model']}_humanoid.xml")
-            root_trans_offset = torch.from_numpy(new_trans) + skeleton_tree.local_translation[0]
+            root_trans_offset = torch.from_numpy(trans_isaac) + skeleton_tree.local_translation[0]
 
             new_sk_state = SkeletonState.from_rotation_and_root_translation(
                         skeleton_tree,  # This is the wrong skeleton tree (location wise) here, but it's fine since we only use the parent relationship here. 
@@ -170,20 +198,25 @@ if __name__ == "__main__":
             new_motion_out['pose_quat_global'] = pose_quat_global
             new_motion_out['pose_quat'] = pose_quat
             new_motion_out['root_trans_offset'] = root_trans_offset
-
+            new_motion_out['pose_aa'] = poses_isaac
+            # FQ features
             new_motion_out['T_c2g'] = np.tile(T_c2g.reshape(1,16), (N,1))
             new_motion_out["kp2d"] = kp2d[:N].reshape(N,23*3)
             new_motion_out["bbox"] = bbox[:N]
             new_motion_out["img_feat"] = img_feat[:N]
-
-            new_motion_out['fps'] = 30
-            new_motion_out['gender'] = "neutral"
-            new_motion_out['pose_aa'] = poses_aa
-            new_motion_out['trans_orig'] = new_trans
-            new_motion_out['beta'] = betas
+            # additional
+            new_motion_out['fps']     = 30
+            new_motion_out['gender']  = "neutral"
+            new_motion_out['betas']   = betas
+            new_motion_out['poses_g'] = poses_g
+            new_motion_out['trans_g'] = trans_g
+            
 
             behave_full_motion_dict[name+"_%d" % i] = new_motion_out
 
 os.makedirs("data/behave_ground", exist_ok=True)
 if upright_start:
-    joblib.dump(behave_full_motion_dict, "data/behave_ground/behave.pkl", compress=True)
+    if debug_data:
+        joblib.dump(behave_full_motion_dict, "data/behave_ground/behave_debug.pkl", compress=True)
+    else:
+        joblib.dump(behave_full_motion_dict, "data/behave_ground/behave.pkl", compress=True)
